27c5be84c107a1f3bf9263cbd0393237f6788ca7
Timothy-timm <temironkogoya123@gmail.com>
2023-06-13 20:37:42 +0700

    perbaikan link boostrapp

--- resources/views/Data-Admin/sekertaris/admin.blade.php
+++ resources/views/Data-Admin/sekertaris/admin.blade.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title> Sistem Informasi | Pemunda GIDI</title>
+  
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
   <!-- Google Font: Source Sans Pro -->
